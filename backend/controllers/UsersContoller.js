aa8b64ba880f9ee48b132dfd45b4271791e018d5
Lentswe Mamonong <lentswe@purplecosmos.co.za>
2022-05-04 01:08:29 +0200

    Worked on userinfo

--- backend/controllers/UsersContoller.js
+++ backend/controllers/UsersContoller.js
@@ -1,7 +1,17 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require('bcrypt');
+
+
+const Encrypt = async (strVal) => {
+  var hash = await bcrypt.hashSync(strVal, 16);
+  return hash;
+}
+
+const UsersModel = require("../models/UserInfoModel");
 
 const GetAllUsers = asyncHandler(async (req, res) => {
-  res.json({ message: "Getting all system users" });
+  const users = await UsersModel.find();
+  res.status(200).json(users);
 });
 
 const GetUser = asyncHandler(async (req, res) => {
@@ -9,10 +19,23 @@ const GetUser = asyncHandler(async (req, res) => {
 });
 
 const CreateUser = asyncHandler(async (req, res) => {
-  if (!req.body.Message) {
+  if (!req.body.FirstName || !req.body.LastName || !req.body.EmailAddress || !req.body.Password) {
     res.status(400);
-    throw new Error("Please add message");
-  } else res.status(200).json({ message: "Creating a user" });
+    throw new Error("Please make sure to enter every field!");
+  } else {
+
+    let password = await Encrypt(req.body.Password);
+    const user = await UsersModel.create({
+      FirstName: req.body.FirstName,
+      LastName: req.body.LastName,
+      EmailAddress: req.body.EmailAddress,
+      Password: password,
+      EmailConfirmed: true,
+      LoginCookie: "",
+      IsDeleted: false
+    });
+    res.status(200).json(user);
+  }
 });
 
 const UpdateUser = asyncHandler(async (req, res) => {
